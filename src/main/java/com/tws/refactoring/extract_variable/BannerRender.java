42e5aaaf17a5edee0a9d015757a5653e38e59246
Mo Hon Sing <mohonsing13@gmail.com>
2018-11-26 22:32:54 +0800

    change indexOf to contains in BannerRender

--- src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -2,10 +2,13 @@ package com.tws.refactoring.extract_variable;
 
 public class BannerRender {
     void renderBanner(String platform, String browser) {
-        if ((platform.toUpperCase().indexOf("MAC") > -1) &&
-                (browser.toUpperCase().indexOf("IE") > -1)) {
+        if ((platform.toUpperCase().contains("MAC")) &&
+                (browser.toUpperCase().contains("IE"))) {
             System.out.print("Banner");
-            // do something
         }
+
+
     }
+
+
 }
