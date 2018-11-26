6b4255516bd7b64e069f27d75b8bbabe6172e958
Mo Hon Sing <mohonsing13@gmail.com>
2018-11-26 22:36:13 +0800

    extract methods to containIgnoreCase method in BannerRender

--- src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -2,12 +2,15 @@ package com.tws.refactoring.extract_variable;
 
 public class BannerRender {
     void renderBanner(String platform, String browser) {
-        if ((platform.toUpperCase().contains("MAC")) &&
-                (browser.toUpperCase().contains("IE"))) {
+        if (containIgnoreCase(platform, "MAC") &&
+                (containIgnoreCase(browser, "IE"))) {
             System.out.print("Banner");
         }
 
+    }
 
+    boolean containIgnoreCase(String string, String searchString){
+        return string.toUpperCase().contains(searchString);
     }
 
 
