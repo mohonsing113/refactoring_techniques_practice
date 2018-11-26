151a26da7b9b19cfdfa20a4c52d28eddae29f499
Mo Hon Sing <mohonsing13@gmail.com>
2018-11-26 21:52:28 +0800

    write test cases for OwingPrinter

--- src/test/java/com/tws/refactoring/extract_method/OwingPrinterTest.java
+++ src/test/java/com/tws/refactoring/extract_method/OwingPrinterTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
@@ -23,9 +24,54 @@ public class OwingPrinterTest {
         System.setOut(originalOut);
     }
 
+
+    /*given name is Joe, and empty order
+      when printOwing
+      then print 0 amount owing order
+     */
     @Test
-    public void printOwing() {
-//        System.out.print("hello");
-//        assertEquals("hello", outContent.toString());
+    public void should_print_a_owing_order_with_only_header() {
+        OwingPrinter owingPrinter = new OwingPrinter();
+        owingPrinter.printOwing("Joe", Arrays.asList());
+        assertEquals(
+                "*****************************\n" +
+                        "****** Customer totals ******\n" +
+                        "*****************************\n" +
+                        "name: Joe\n" +
+                        "amount: 0.0\n"
+                , outContent.toString());
+    }
+
+    /*given name is Tommy, have 1 order that is $5
+      when printOwing
+      then print a 5 amount owing order
+     */
+    @Test
+    public void should_print_a_owing_order_with_5_dolloar_amount() {
+        OwingPrinter owingPrinter = new OwingPrinter();
+        owingPrinter.printOwing("Tommy", Arrays.asList(new Order(5)));
+        assertEquals(
+                "*****************************\n" +
+                        "****** Customer totals ******\n" +
+                        "*****************************\n" +
+                        "name: Tommy\n" +
+                        "amount: 5.0\n", outContent.toString());
+    }
+
+
+    /*given name is Sam, have 2 order those are $5 and $10
+      when printOwing
+      then print a 15 amount owing order
+     */
+    @Test
+    public void should_print_a_owing_order_with_15_dollar_amount() {
+        OwingPrinter owingPrinter = new OwingPrinter();
+        owingPrinter.printOwing("Sam", Arrays.asList(new Order(5), new Order(10)));
+        assertEquals(
+                "*****************************\n" +
+                        "****** Customer totals ******\n" +
+                        "*****************************\n" +
+                        "name: Sam\n" +
+                        "amount: 15.0\n", outContent.toString());
     }
 }
