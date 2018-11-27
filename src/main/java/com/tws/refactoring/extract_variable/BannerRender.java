1c7ef3934e0856dbb3bed2c87e753c4daf5afe19
Mo Hon Sing <h.s.mo@oocl.com>
2018-11-27 08:58:44 +0800

    extract check platform and check browser in BannerRender

--- src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -2,16 +2,21 @@ package com.tws.refactoring.extract_variable;
 
 public class BannerRender {
     void renderBanner(String platform, String browser) {
-        if (containIgnoreCase(platform, "MAC") &&
-                (containIgnoreCase(browser, "IE"))) {
+        if (isMac(platform) && isIE(browser)){
             System.out.print("Banner");
         }
-
     }
 
-    boolean containIgnoreCase(String string, String searchString){
+    boolean isMac(String platform) {
+        return containIgnoreCase(platform, "MAC");
+    }
+
+    boolean isIE(String browser) {
+        return containIgnoreCase(browser, "IE");
+    }
+
+    boolean containIgnoreCase(String string, String searchString) {
         return string.toUpperCase().contains(searchString);
     }
-
 
 }
